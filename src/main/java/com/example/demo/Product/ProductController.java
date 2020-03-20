d9e95c40bcbbef625a2df8f77803049933988f03
ybx <401129874@qq.com>
2020-03-20 21:54:53 +0800

    增加了cookie

--- src/main/java/com/example/demo/Product/ProductController.java
+++ src/main/java/com/example/demo/Product/ProductController.java
@@ -5,18 +5,40 @@ import com.example.demo.Response.Result;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.util.List;
 import java.util.Optional;
 
 @RestController
-@CrossOrigin
+@CrossOrigin(allowCredentials = "true",origins = {"*"})
 public class ProductController {
     @Autowired
     ProductService productService;
     //查出所有的产品
-    @CrossOrigin
     @RequestMapping(value = "/get/product/All",method = RequestMethod.GET)
-    public Result selectAllProduct(){
+    public Result selectAllProduct(HttpServletRequest request){
+        Cookie[] cookies = request.getCookies();
+        String sessionId = "";
+        if(cookies == null || cookies.length == 0){
+            System.out.println("cookies为空");
+        }else {
+            for (Cookie str : cookies) {
+                if(str.getName().equals("email")){
+                   sessionId= str.getValue();
+                }
+                System.out.println(str.getName() + " " + str.getValue());
+            }
+        }
+//        Object attribute = request.getSession().getAttribute(sessionId);
+//        System.out.println((String)attribute);
+//        if(cookie == null || cookie == ""){
+//            System.out.println("cookie为空");
+//        }else {
+//            System.out.println(cookie);
+//        }
         List<Product> products = productService.selectAll();
         if(products != null) {
             return new Result(Msg.SelectAllProductSuccess,products);
@@ -25,18 +47,17 @@ public class ProductController {
         }
     }
     //根据ID查询产品
-    @CrossOrigin
     @RequestMapping(value = "/get/product/{Id}",method = RequestMethod.GET)
-    public Result selectProductById(@PathVariable("Id") int id){
+    public Result selectProductById(@PathVariable("Id") int id, @CookieValue("loginUser") String cookie){
         Optional<Product> product = productService.selectProductById(id);
         if(product != null) {
+            System.out.println(cookie);
             return new Result(Msg.SelectProductByIdSuccess,product);
         }else {
             return new Result(Msg.SelectProductByIdFailed);
         }
     }
     //添加或者修改商品
-    @CrossOrigin
     @RequestMapping(value = "/post/produce/updateOrAdd",method = RequestMethod.POST)
     public Result updateOrAddProduct(@RequestBody Product product){
         Product product1 = productService.updateOrAdd(product);
--- src/main/java/com/example/demo/User/UserController.java
+++ src/main/java/com/example/demo/User/UserController.java
@@ -1,37 +1,67 @@
 package com.example.demo.User;
 
+import com.alibaba.fastjson.JSON;
 import com.example.demo.Response.Msg;
 import com.example.demo.Response.Result;
 import com.example.demo.Util.EmailUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.websocket.Session;
 import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 
 @RestController
+@CrossOrigin(allowCredentials = "true",origins = {"*"})
 public class UserController {
     @Autowired
     UserService userService;
-    @CrossOrigin
     @RequestMapping(value = "/java/login",method = RequestMethod.POST)
     public Result login(@RequestBody Map<String,String> params, HttpServletResponse response) {
-        response.setHeader("Access-Control-Allow-Origin","*");
         String email = params.get("email");
         String pass = params.get("pass");
+//        HttpSession session = request.getSession();
+//        session.setAttribute(email,email + "的信息");
         User byEmailAndPass = userService.findByEmailAndPass(email, pass);
         if (byEmailAndPass == null) {
             System.out.println("登录失败");
             return new Result(Msg.EmailOrPassError);
         } else {
+            Cookie cookie = new Cookie("email", email);
+            //setPath()设置可以访问cookie的路径
+            cookie.setPath("/");
+            //设置跨域
+//            cookie.setDomain(".*");
+            //设置cookie的时长,10分钟
+            cookie.setMaxAge(10*60);
+            response.addCookie(cookie);
+            Cookie cookie1 = new Cookie("email1", email);
+            //setPath()设置可以访问cookie的路径
+            cookie1.setPath("/");
+            //设置跨域
+//            cookie.setDomain(".*");
+            cookie1.setMaxAge(10*60);
+            response.addCookie(cookie1);
+            Cookie cookie2 = new Cookie("email2", email);
+            //setPath()设置可以访问cookie的路径
+            cookie2.setPath("/");
+            //设置跨域
+//            cookie.setDomain(".*");
+            //设置cookie的时长,10分钟
+            cookie2.setMaxAge(10*60);
+            response.addCookie(cookie2);
+            //设置cookie的时长,10分钟
+
             System.out.println("登录成功");
             return new Result(Msg.LoginSuccess);
         }
     }
-    @CrossOrigin
     @RequestMapping("/java/regist")
     public Result regist(@RequestBody Map<String,String> params) {
         String email = params.get("email");
